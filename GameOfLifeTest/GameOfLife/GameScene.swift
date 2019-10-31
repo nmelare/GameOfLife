5b52597c2d626508890e962953df7db749ae400d
Nathalia <nathaliamelare@gmail.com>
2019-10-31 16:41:13 -0300

    Adicionado grid

---
+++ GameOfLifeTest/GameOfLife/GameScene.swift
@@ -0,0 +1,38 @@
+//
+//  GameScene.swift
+//  GameOfLife
+//
+//  Created by Nathalia Melare on 31/10/19.
+//  Copyright © 2019 Nathalia Melare. All rights reserved.
+//
+
+import Foundation
+import SceneKit
+
+class GameScene: SCNScene {
+    
+    override init() {
+        super.init()
+        
+          let box = SCNBox(width: 0.6 , height: 0.6,
+                                   length: 0.1, chamferRadius: 0.005)
+            box.firstMaterial?.diffuse.contents = UIColor.red
+//            box.firstMaterial?.specular.contents = UIColor.white
+            box.firstMaterial?.emission.contents = UIColor.darkGray
+            let boxnode = SCNNode(geometry: box)
+            let offset: Int = 10
+
+            for xIndex:Int in 0...32 {
+                for yIndex:Int in 0...32 {
+                    let boxCopy = boxnode.copy() as! SCNNode
+                    boxCopy.position.x = Float(xIndex - offset)
+                    boxCopy.position.y = Float(yIndex - offset)
+                    self.rootNode.addChildNode(boxCopy)
+                }
+            }
+        }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
